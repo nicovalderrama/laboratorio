18cdfcac6957349f142f52fa0d48924f80bd6879
nicolas <nicovalderrama2002@gmail.com>
2023-11-13 17:01:53 -0300

    inicio proyecto

---
+++ main.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+typedef struct Mesa{
+    int num_mesa;
+    struct Partido[5];
+    int votos_en_blanco;
+    int votos_impugnados;
+} Mesa;
+
+typedef struct Partido{
+    char nombre_partido[30];
+    int votos_presidente;
+    int votos_gobernador;
+} Partido;
+
+
+int main() {
+
+}
